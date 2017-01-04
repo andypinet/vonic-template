7068226dd35d23e1add46d4dfe61a748a7c374c5
andypinet <lbc19920631@163.com>
2017-01-04 09:12:40 +0800

    init

---
+++ template/src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="page has-navbar" v-nav="{ title: 'Home' }">
+    <div class="page-content text-center">
+      <p class="padding">{{ msg }}</p>
+
+      <a class="button button-assertive" v-link="{path: '/about'}">
+        about
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vonic!'
+    }
+  }
+}
+</script>
